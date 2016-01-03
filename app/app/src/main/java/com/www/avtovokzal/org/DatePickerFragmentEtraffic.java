c76ed0d2efdcee315024865db5025cd7642a8524
Sergey Uharskiy <uharskiy@gmail.com>
2016-01-03 10:31:36 +0500

    Fix Samsung dialog bug

--- app/app/src/main/java/com/www/avtovokzal/org/DatePickerFragmentEtraffic.java
+++ app/app/src/main/java/com/www/avtovokzal/org/DatePickerFragmentEtraffic.java
@@ -2,9 +2,12 @@ package com.www.avtovokzal.org;
 
 import android.app.DatePickerDialog;
 import android.app.Dialog;
+import android.content.Context;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v4.app.DialogFragment;
+import android.support.v7.view.ContextThemeWrapper;
 
 import java.util.Calendar;
 
@@ -13,11 +16,28 @@ public class DatePickerFragmentEtraffic extends DialogFragment {
     @NonNull
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
+        Context context = getActivity();
+
         final Calendar c = Calendar.getInstance();
         int year = c.get(Calendar.YEAR);
         int month = c.get(Calendar.MONTH);
         int day = c.get(Calendar.DAY_OF_MONTH);
 
-        return new DatePickerDialog(getActivity(), (EtrafficActivity)getActivity(), year, month, day);
+        if (isBrokenSamsungDevice()) {
+            context = new ContextThemeWrapper(getActivity(), android.R.style.Theme_Holo_Light_Dialog);
+        }
+
+        return new DatePickerDialog(context, (EtrafficActivity)getActivity(), year, month, day);
+    }
+
+    private static boolean isBrokenSamsungDevice() {
+        return (Build.MANUFACTURER.equalsIgnoreCase("samsung")
+                && isBetweenAndroidVersions(
+                Build.VERSION_CODES.LOLLIPOP,
+                Build.VERSION_CODES.LOLLIPOP_MR1));
+    }
+
+    private static boolean isBetweenAndroidVersions(int min, int max) {
+        return Build.VERSION.SDK_INT >= min && Build.VERSION.SDK_INT <= max;
     }
 }
